aab6c51c9a92607eb3eb81d19c79f960843fcf7f
jincheol <oscar4293@gmail.com>
2024-06-10 20:50:47 +0900

    update

--- gcn/test.py
+++ gcn/test.py
@@ -23,8 +23,8 @@ torch.use_deterministic_algorithms(False)
 epochs=200
 learning_rate=0.01
 weight_decay=5e-4
-hidden_unit=32
-dropout_rate=0.5
+hidden_unit=16
+dropout_rate=0.3
 
 dataset = Planetoid(root='/tmp/Cora', name='Cora')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
